d1d06e6e56f2b596978789625912024fa5cdcac3
Ricky Cheung <rcheung844@gmail.com>
2021-01-28 21:53:30 +0800

    A37: Add Yamaha calibration module
     * For yas533-mag
    
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- A37/A37-vendor.mk
+++ A37/A37-vendor.mk
@@ -92,6 +92,7 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/A37/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/oppo/A37/proprietary/vendor/lib/libc2d30-a4xx.so:system/vendor/lib/libc2d30-a4xx.so \
     vendor/oppo/A37/proprietary/vendor/lib/libcalmodule_memsic.so:system/vendor/lib/libcalmodule_memsic.so \
+    vendor/oppo/A37/proprietary/vendor/lib/libcalmodule_yamaha.so:system/vendor/lib/libcalmodule_yamaha.so \
     vendor/oppo/A37/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/oppo/A37/proprietary/vendor/lib/libcneapiclient.so:system/vendor/lib/libcneapiclient.so \
     vendor/oppo/A37/proprietary/vendor/lib/libchromatix_imx179_15399_common.so:system/vendor/lib/libchromatix_imx179_15399_common.so \
